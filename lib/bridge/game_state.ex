0e33f69b0d2ed6c8ec60122fd11fecc93904f3d0
Jeff Ching <ching.jeff@gmail.com>
2015-11-02 16:10:52 -0800

    genserver for gamestate

--- lib/bridge/game_state.ex
+++ lib/bridge/game_state.ex
@@ -1,5 +1,41 @@
 defmodule Bridge.GameState do
+  use GenServer
 
-  defstruct vulnerable: false, points_below: 0, points_above: 0
+  defmodule HandHistory do
+    defstruct contract: nil, tricks_made: 0, results: nil
+  end
+
+  defstruct hands: [], current_contract: nil
+
+  ## Client API
+
+  @doc """
+  Starts the game.
+  """
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def set_contract(server, contract) do
+    GenServer.call(server, {:set_contract, contract})
+  end
+
+  def get_contract(server) do
+    GenServer.call(server, {:get_contract})
+  end
+
+  ## Server Callbacks
+
+  def init(:ok) do
+    {:ok, %Bridge.GameState{}}
+  end
+
+  def handle_call({:set_contract, contract}, _from, state) do
+    state = %Bridge.GameState{ state | current_contract: contract }
+    {:reply, contract, state}
+  end
+  def handle_call({:get_contract}, _from, state) do
+    {:reply, state.current_contract, state}
+  end
 
 end
